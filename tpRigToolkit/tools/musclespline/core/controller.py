6f9fc86d33bf928ce480e9c96208cce7c1114c93
Tomas Poveda <tpovedatd@gmail.com>
2020-09-07 01:02:02 +0200

    Implemented basic controller functions

--- tpRigToolkit/tools/musclespline/core/controller.py
+++ tpRigToolkit/tools/musclespline/core/controller.py
@@ -7,12 +7,7 @@ Muscle Spline  widget controller class implementation
 
 from __future__ import print_function, division, absolute_import
 
-import os
-
 import tpDcc as tp
-from tpDcc.libs.python import jsonio
-
-from tpRigToolkit.tools.interpolateit.widgets import interpolator
 
 LOGGER = tp.LogsMgr().get_logger('tpRigToolkit-tools-musclespline')
 
@@ -31,3 +26,57 @@ class MuscleSplineController(object):
     @property
     def model(self):
         return self._model
+
+    def change_name(self, value):
+        self._model.name = value
+
+    def change_size(self, value):
+        self._model.size = value
+
+    def change_insertion_controls(self, value):
+        self._model.insertion_controls = value
+
+    def change_number_driven_joints(self, value):
+        self._model.driven_joints = value
+
+    def change_constraint_mid_controls(self, flag):
+        self._model.constraint_mid_controls = flag
+
+    def change_lock_controls_scale(self, flag):
+        self._model.lock_controls_scale = flag
+
+    def change_enable_advanced(self, flag):
+        self._model.enable_advanced = flag
+
+    def change_control_suffix(self, value):
+        self._model.control_suffix = value
+
+    def change_joint_suffix(self, value):
+        self._model.joint_suffix = value
+
+    def change_group_suffix(self, value):
+        self._model.group_suffix = value
+
+    def change_driven_suffix(self, value):
+        self._model.driven_suffix = value
+
+    def change_main_muscle_set_name(self, value):
+        self._model.main_muscle_set_name = value
+
+    def change_muscle_set_suffix(self, value):
+        self._model.muscle_set_suffix = value
+
+    def change_muscle_spline_name(self, value):
+        self._model.muscle_spline_name = value
+
+    def change_controls_group_suffix(self, value):
+        self._model.controls_group_suffix = value
+
+    def change_joints_group_suffix(self, value):
+        self._model.joints_group_suffix = value
+
+    def change_root_group_suffix(self, value):
+        self._model.root_group_suffix = value
+
+    def change_auto_group_suffix(self, value):
+        self._model.auto_group_suffix = value
